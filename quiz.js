613b2a687d4d9f079e1d1f20fb1c84f687c92040
KentToha <kenttoha@hotmail.com>
2017-10-26 14:50:34 +0100

    quiz.html

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-
+	let question = [];
 	let quiz = {
 		getQuiz: function () {
 			console.log("Loading quiz");
@@ -7,6 +7,7 @@ $(document).ready(() => {
 			$.getJSON(url, function (data) {
 				console.log("Retrieved quiz");
 				console.log(data);
+				questions = data.results
 				let Question = data.results[0].question;
 				console.log(Question);
 				$('#Question').text(Question);
@@ -20,11 +21,17 @@ $(document).ready(() => {
 
 			})
 		}
-	}
-	let viewQuestions = {
 
 	}
-	
 
-		quiz.getQuiz()
+	$('#confirmAnswer').on('click', () => {
+		let selectedAnswer = $('input:checked').next().text();
+		console.log("selectedanswer : " + selectedAnswer);
+		if (selectedAnswer === questions.correct_answer) {
+
+		} else {
+
+		}
 	})
+	quiz.getQuiz()
+})
