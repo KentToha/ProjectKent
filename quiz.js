3ceeedaf94ebbafcbd8967ce4643bb800d6f7605
KentToha <kenttoha@hotmail.com>
2017-10-26 12:50:41 +0100

    quiz.js

---
+++ quiz.js
@@ -0,0 +1,30 @@
+
+$(document).ready(() => {
+
+	let quiz = {
+		getQuiz: function () {
+			console.log("Loading quiz");
+			let url = "https://opentdb.com/api.php?amount=4&category=21&type=multiple";
+			$.getJSON(url, function (data) {
+				console.log("Retrieved quiz");
+				console.log(data);
+				let answer = data.results[0].correct_answer;
+				console.log(answer);
+				$('#choice1').text(answer)
+				let incorrect_answers = data.results[0].incorrect_answers;
+				$('#choice2').text(incorrect_answers[0])
+				$('#choice3').text(incorrect_answers[1])
+				$('#choice4').text(incorrect_answers[2])
+
+			})
+		}
+	}
+	let viewQuestions = {
+
+	}
+	
+
+		quiz.getQuiz()
+	})
+
+
